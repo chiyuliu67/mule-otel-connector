06623397aa23895f6c635f74e90c821213defac9
Joaquin Esteban Ponte Diaz <joaquin.ponte@gmail.com>
2022-12-29 01:23:43 -0700

    Removing unnecessary validations

--- src/main/java/com/mulesoft/ot/interceptor/InitialInterceptor.java
+++ src/main/java/com/mulesoft/ot/interceptor/InitialInterceptor.java
@@ -5,20 +5,10 @@ import org.mule.runtime.api.interception.ProcessorInterceptorFactory;
 import org.springframework.stereotype.Component;
 
 /**
- * Will intercept the root processor flow. This class must be registered in
- * registry-bootstrap.properties
- *
- * <p>
- * Disable tracing by setting "otel.mule.enabletracing" to `false`.
- *
- * <p>
- * See registry-bootstrap.properties.
+ * Will intercept the root processor flow. This class must be registered in registry-bootstrap.properties
  */
 @Component
 public class InitialInterceptor implements ProcessorInterceptorFactory {
-
-    public static final String OTEL_MULE_ENABLETRACING = "otel.mule.enabletracing";
-    private final boolean enableTracing = Boolean.parseBoolean(System.getProperty(OTEL_MULE_ENABLETRACING, "true"));
 
     @Override
     public org.mule.runtime.api.interception.ProcessorInterceptor get() {
@@ -27,13 +17,9 @@ public class InitialInterceptor implements ProcessorInterceptorFactory {
 
     /**
      * This intercepts the first processor of root container
-     *
-     * @param location
-     *            {@link ComponentLocation}
-     * @return true if intercept
      */
     @Override
     public boolean intercept(ComponentLocation location) {
-        return enableTracing && location.getLocation().endsWith("/0");
+        return true;
     }
 }
